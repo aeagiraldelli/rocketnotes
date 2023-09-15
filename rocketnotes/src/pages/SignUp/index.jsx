09ba253eb00338ca4a9bc09ef6989b47f2d43035
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2023-09-15 14:38:06 -0300

    signup page

--- rocketnotes/src/pages/SignUp/index.jsx
+++ rocketnotes/src/pages/SignUp/index.jsx
@@ -1,4 +1,4 @@
-import { FiLock, FiMail } from 'react-icons/fi';
+import { FiLock, FiMail, FiUser } from 'react-icons/fi';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
@@ -6,21 +6,22 @@ import { ButtonText } from '../../components/ButtonText';
 
 import { Container, Form, Background } from './style';
 
-export function SignIn() {
+export function SignUp() {
   return (
     <Container>
+      <Background />
       <Form>
         <h1>Rocket Notes</h1>
         <p>Aplicação para gerenciar seus links.</p>
-        <h2>Faça seu login</h2>
+        <h2>Crie sua conta</h2>
+        <Input placeholder="Nome" type="text" icon={FiUser} />
         <Input placeholder="e-mail" type="email" icon={FiMail} />
         <Input placeholder="senha" type="password" invertReveal icon={FiLock} />
-        <Button label="Login" />
+        <Button label="Cadastrar" />
         <span className="button-text">
-          <ButtonText title="Criar conta" isActive />
+          <ButtonText title="Voltar para tela de login" isActive />
         </span>
       </Form>
-      <Background />
     </Container>
   );
 }
