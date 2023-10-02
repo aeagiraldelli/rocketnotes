d78c896923f96e570d143ae0aebba11d32642cbf
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2023-10-02 20:55:33 -0300

    save note

--- rocketnotes/src/pages/New/index.jsx
+++ rocketnotes/src/pages/New/index.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { api } from '../../services/api';
 
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
@@ -11,11 +13,16 @@ import { NoteItem } from '../../components/NoteItem';
 import { Container, Form } from './style';
 
 export function New() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
   const [links, setLinks] = useState([]);
   const [newLink, setNewLink] = useState('');
 
   const [tags, setTags] = useState([]);
   const [newTag, setNewTag] = useState('');
+
+  const navitage = useNavigate();
 
   function handleAddTag() {
     setTags((prevState) => [...prevState, newTag]);
@@ -35,6 +42,29 @@ export function New() {
     setLinks((prevState) => prevState.filter((link) => link !== linkToRemove));
   }
 
+  async function handleSaveNote() {
+    if (!title || !description) {
+      return alert('Por favor, preencha os campos Título e Observações.');
+    }
+
+    try {
+      await api.post('/notes', {
+        title,
+        description,
+        links,
+        tags
+      });
+      alert('Nota salva com sucesso!');
+      navitage('/');
+    } catch (error) {
+      if (error.response) {
+        return alert(error.response.data.message);
+      } else {
+        return alert('Erro salvar nota. Tente novamente mais tarde.');
+      }
+    }
+  }
+
   return (
     <Container>
       <Header />
@@ -44,42 +74,27 @@ export function New() {
             <h1>Criar nota</h1>
             <Link to="/">voltar</Link>
           </header>
-          <Input placeholder="Título" />
-          <TextArea placeholder="Observações" />
+          <Input placeholder="Título" onChange={(e) => setTitle(e.target.value)} />
+          <TextArea placeholder="Observações" onChange={(e) => setDescription(e.target.value)} />
           <Section title="Links úteis">
             {
               links.map((link, index) => {
-                return <NoteItem
-                  key={`${index}-${link}`}
-                  value={link}
-                  onClick={() => handleRemoveLink(link)}
-                />
+                return <NoteItem key={`${index}-${link}`} value={link} onClick={() => handleRemoveLink(link)} />
               })
             }
-            <NoteItem isNew placeholder="novo link" value={newLink}
-              onChange={(e) => setNewLink(e.target.value)}
-              onClick={handleAddLink}
-            />
+            <NoteItem isNew placeholder="novo link" value={newLink} onChange={(e) => setNewLink(e.target.value)} onClick={handleAddLink} />
           </Section>
           <Section title="Marcadores">
             <div className="tag-list">
               {
                 tags.map((tag, index) => {
-                  return <NoteItem
-                    key={`${index}-${tag}`}
-                    className="tag__item"
-                    value={tag}
-                    onClick={() => handleRemoveTag(tag)}
-                  />
+                  return <NoteItem key={`${index}-${tag}`} className="tag__item" value={tag} onClick={() => handleRemoveTag(tag)} />
                 })
               }
-              <NoteItem className="tag__item" isNew placeholder="nova tag" value={newTag}
-                onChange={(e) => setNewTag(e.target.value)}
-                onClick={handleAddTag}
-              />
+              <NoteItem className="tag__item" isNew placeholder="nova tag" value={newTag} onChange={(e) => setNewTag(e.target.value)} onClick={handleAddTag} />
             </div>
           </Section>
-          <Button label="Salvar" />
+          <Button label="Salvar" onClick={handleSaveNote} />
         </Form>
       </main>
     </Container>
